9599ac8d2b26e38e65386d77c093cb13b60ff4b1
LS <the413reply@gmail.com>
2024-05-02 16:10:57 +0800

    Update site.config.js
    add 🆕 config

--- config/site.config.js
+++ config/site.config.js
@@ -6,8 +6,10 @@ module.exports = {
   // This is what we use to identify who you are when you are initialising the website for the first time.
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
-  // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'exportusr@mdjnu413.onmicrosoft.com',
+  // // your email being exposed in public.
+  // userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'exportusr@mdjnu413.onmicrosoft.com',
+  
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'exportusr@vxfmx.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
@@ -19,8 +21,10 @@ module.exports = {
   // The name of your website. Present alongside your icon.
   title: "413's OneDrive",
 
-  // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  // // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
+  // baseDirectory: '/Public',
+  
+  baseDirectory: '\BackupE5\Public',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
