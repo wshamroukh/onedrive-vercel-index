d9a85673e3f23c80b024e19e1e5be49270357a1d
LS <the413reply@gmail.com>
2024-11-18 17:31:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -47,8 +47,9 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   // protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
-  protectedRoutes: ['/BackupE5/Public/Saved',
-                     ],
+  protectedRoutes: ['/BackupE5/Public/🎓毕业照片', 
+                   ],
+
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
