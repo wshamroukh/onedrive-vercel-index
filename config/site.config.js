6bfbe9b67c5f7cd09b3adf195ec0ceb416e71de5
wshamroukh <wshamroukh@gmail.com>
2025-02-17 19:43:55 +0400

    Update site config

--- config/site.config.js
+++ config/site.config.js
@@ -19,12 +19,12 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "413's OneDrive",
+  title: "Waddah's OneDrive",
 
   // // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   // baseDirectory: '/Public',🧩🧩🧩🧩🧩
   
-  baseDirectory: '/BackupE5/Public',
+  baseDirectory: '/Xiaomi-Mi10-Ultra-ROMs',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
